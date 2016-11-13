d981f9791ea3033472d72cc519d8e10e67becf43
wastegas <wastegas@gmail.com>
2016-11-13 09:52:12 -1000

    add autoconf generated files

--- include/exampleapp.h
+++ include/exampleapp.h
@@ -3,7 +3,7 @@
 
 #include <gtk/gtk.h>
 
-#define EXAMPLE_APP_TYPE(example_app_get_type())
+#define EXAMPLE_APP_TYPE (example_app_get_type ())
 G_DECLARE_FINAL_TYPE(ExampleApp, example_app, EXAMPLE, APP, GtkApplication)
 
 ExampleApp *example_app_new (void);
--- include/exampleappwin.h
+++ include/exampleappwin.h
@@ -4,7 +4,7 @@
 #include <gtk/gtk.h>
 #include "exampleapp.h"
 
-#define EXAMPLE_APP_WINDOW_TYPE(example_app_window_get_type())
+#define EXAMPLE_APP_WINDOW_TYPE (example_app_window_get_type ())
 G_DECLARE_FINAL_TYPE(ExampleAppWindow, example_app_window, EXAMPLE, APP_WINDOW, GtkApplicationWindow)
 
 ExampleAppWindow  *example_app_window_new (ExampleApp *app);
