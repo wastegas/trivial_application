91be7a5c05d925f165e05f47f7a5608c30df0626
wastegas <wastegas@gmail.com>
2016-11-16 21:30:45 -1000

    add support to open files

--- src/exampleappwin.c
+++ src/exampleappwin.c
@@ -8,7 +8,17 @@ struct _ExampleAppWindow
   GtkApplicationWindow parent;
 };
 
-G_DEFINE_TYPE(ExampleAppWindow, example_app_window, GTK_TYPE_APPLICATION_WINDOW);
+//G_DEFINE_TYPE(ExampleAppWindow, example_app_window, GTK_TYPE_APPLICATION_WINDOW);
+
+typedef struct _ExampleAppWindowPrivate ExampleAppWindowPrivate;
+
+struct _ExampleAppWindowPrivate
+{
+  GtkWidget *stack;
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE(ExampleAppWindow, example_app_window, GTK_TYPE_APPLICATION_WINDOW);
+
 
 static void
 example_app_window_init(ExampleAppWindow *app)
@@ -21,6 +31,8 @@ example_app_window_class_init(ExampleAppWindowClass *class)
 {
   gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS (class),
                                               "/Users/Ronnie/Documents/gtk/trivial_application/src/window.ui");
+  gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS (class),
+                                         ExampleAppWindow, stack);
 }
 
 ExampleAppWindow *
@@ -36,4 +48,34 @@ void
 example_app_window_open(ExampleAppWindow *win,
                         GFile *file)
 {
+  ExampleAppWindowPrivate *priv;
+  gchar *basename;
+  GtkWidget *scrolled, *view;
+  gchar *contents;
+  gsize length;
+
+  priv = example_app_window_get_instance_private(win);
+  basename = g_file_get_basename(file);
+
+  scrolled = gtk_scrolled_window_new(NULL, NULL);
+  gtk_widget_show(scrolled);
+  gtk_widget_set_hexpand(scrolled, TRUE);
+  gtk_widget_set_vexpand(scrolled, TRUE);
+  view = gtk_text_view_new();
+  gtk_text_view_set_editable(GTK_TEXT_VIEW(view), FALSE);
+  gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(view), FALSE);
+  gtk_widget_show(view);
+  gtk_container_add(GTK_CONTAINER(scrolled), view);
+  gtk_stack_add_titled(GTK_STACK(priv->stack), scrolled, basename, basename);
+
+  if(g_file_load_contents(file, NULL, &contents, &length, NULL, NULL))
+    {
+      GtkTextBuffer *buffer;
+      
+      buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
+      gtk_text_buffer_set_text(buffer, contents, length);
+      g_free(contents);
+    }
+
+  g_free(basename);
 }
