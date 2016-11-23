295200431f4fbd4d0408247ae11a73d1dcf973b2
wastegas <wastegas@gmail.com>
2016-11-22 20:12:10 -1000

    fix typo

--- include/exampleappprefs.h
+++ include/exampleappprefs.h
@@ -4,7 +4,7 @@
 #include <gtk/gtk.h>
 #include "exampleappwin.h"
 
-#define EXAMPLE_APP_PREFS_TYPE (example_app_pref_get_type())
+#define EXAMPLE_APP_PREFS_TYPE (example_app_prefs_get_type())
 G_DECLARE_FINAL_TYPE (ExampleAppPrefs, example_app_prefs, EXAMPLE, APP_PREFS, GtkDialog)
 
 ExampleAppPrefs   *example_app_prefs_new    (ExampleAppWindow *win);
